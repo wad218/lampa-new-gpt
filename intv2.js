dac2436dc43e17f72c797d5b2024bcb8103f6d99
wad218 <wad.2015.98@gmail.com>
2026-03-04 15:37:04 +0200

    Update intv2.js

--- intv2.js
+++ intv2.js
@@ -834,7 +834,7 @@
                 <div class="full-start__rate rate--tmdb">
                     <div>${vote}</div>
                     <div class="source--name">
-                        <img src="https://raw.githubusercontent.com/wad218/lmp-rtg/main/wwwroot/tmdb.svg" style="height:18px;">
+                        <img src="https://raw.githubusercontent.com/wad218/lmp-rtg/main/wwwroot/tmdb.svg" style="height:20px;">
                     </div>
                 </div>
             `);
