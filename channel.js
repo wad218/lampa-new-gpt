9aabf07d950fb3292107b880f51b16a33b33048c
wad218 <wad.2015.98@gmail.com>
2026-03-14 20:32:58 +0200

    Update channel.js

--- channel.js
+++ channel.js
@@ -1,21 +1,41 @@
 (function () {
     'use strict';
 
-    if (!window.Lampa) return;
+    if (typeof Lampa === 'undefined') return;
 
-    Lampa.Component.add('history_view', {
-        component: 'category',
+    function loadHistory(component, params, oncomplete){
+        let hist = [];
+
+        try{
+            let fav = Lampa.Favorite.all();
+            if(fav && fav.history) hist = fav.history;
+        }
+        catch(e){}
+
+        oncomplete({
+            results: hist,
+            title: 'Переглянуте',
+            params:{
+                items:{
+                    mapping:'line',
+                    view:15
+                }
+            }
+        });
+    }
+
+    Lampa.Api.sources.history_home = {
         title: 'Переглянуте',
-        source: 'history'
-    });
+        main: loadHistory
+    };
 
     Lampa.Listener.follow('app', function(e){
         if(e.type === 'ready'){
 
             Lampa.Home.add({
                 title: 'Переглянуте',
-                component: 'history_view'
-            }, 0);   // ← це робить канал першим
+                source: 'history_home'
+            }, 0);
 
         }
     });
