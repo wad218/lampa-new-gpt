a27e38291a491a19d8a7869ebd34b0a53ec4a286
wad218 <wad.2015.98@gmail.com>
2026-03-14 21:19:03 +0200

    Update channel.js

--- channel.js
+++ channel.js
@@ -3,46 +3,21 @@
 
 if (typeof Lampa === 'undefined') return;
 
-// функція отримання історії (логіка як у ymain.js)
-function loadHistoryRow(callback){
+// створюємо компонент історії
+Lampa.Component.add('history_home', {
+    component: 'category_full',
+    source: 'history'
+});
 
-    let hist = [];
-
-    try{
-        let fav = Lampa.Favorite.all();
-        if(fav && fav.history) hist = fav.history;
-    }
-    catch(e){}
-
-    if(!hist.length){
-        callback({results:[]});
-        return;
-    }
-
-    // стандартні картки Lampa
-    callback({
-        title: 'Переглянуте',
-        results: hist,
-        params:{
-            items:{
-                mapping: 'line',
-                view: 15
-            }
-        }
-    });
-
-}
-
-// додаємо рядок у головну
+// додаємо рядок на головну
 Lampa.Listener.follow('app', function(e){
 
     if(e.type === 'ready'){
 
-        Lampa.Api.partNext([
-            function(cb){
-                loadHistoryRow(cb);
-            }
-        ],0);
+        Lampa.Home.add({
+            title: 'Переглянуте',
+            component: 'history_home'
+        },0);
 
     }
 
