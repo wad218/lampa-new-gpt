ad8aa664d0270a3dc9174d0892639439d2b965e5
wad218 <wad.2015.98@gmail.com>
2026-03-14 21:29:38 +0200

    Update channel.js

--- channel.js
+++ channel.js
@@ -3,21 +3,27 @@
 
 if (typeof Lampa === 'undefined') return;
 
-// створюємо компонент історії
 Lampa.Component.add('history_home', {
     component: 'category_full',
     source: 'history'
 });
 
-// додаємо рядок на головну
 Lampa.Listener.follow('app', function(e){
 
     if(e.type === 'ready'){
 
-        Lampa.Home.add({
-            title: 'Переглянуте',
-            component: 'history_home'
-        },0);
+        setTimeout(function(){
+
+            if(Lampa.Home){
+
+                Lampa.Home.add({
+                    title: 'Переглянуте',
+                    component: 'history_home'
+                });
+
+            }
+
+        },1500);
 
     }
 
