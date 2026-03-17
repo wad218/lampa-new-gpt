d2058e18da599c0d62c2e1442d523165d01a410c
wad218 <wad.2015.98@gmail.com>
2026-03-17 13:57:51 +0200

    Update channel2.js

--- channel2.js
+++ channel2.js
@@ -1,140 +1,100 @@
-(function () {
-    'use strict';
-
-    function startPlugin() {  
-        window.plugin_watched_ready = true;
-      
-        let manifest = {  
-            type: 'video',  
-            version: '1.0.2',  
-            name: Lampa.Lang.translate('title_watched'),  
-            description: 'Shows watched movies and TV series',  
-            component: 'watched'  
-        };
-          
-        Lampa.Manifest.plugins = manifest;
-      
-        // Компонент для відображення сторінки з бокового меню
-        function component(object){  
-            let comp = new Lampa.InteractionMain(object);
-      
-            comp.create = function(){  
-                this.activity.loader(true);
-      
-                let all = Lampa.Favorite.all();
-                let history = all.history || [];
-      
-                // Глибока копія даних, щоб скинути "горизонтальні" налаштування з головного екрана
-                let results = JSON.parse(JSON.stringify(history.slice(0, 50))).map(item => {
-                    let ready = item;
-                    // Очищуємо опис та примусово вимикаємо широкий режим
-                    ready.overview = ''; 
-                    ready.is_wide = false;
-                    // Повертаємо постер, якщо він був замінений на кадр
-                    ready.background_image = item.img || item.poster || item.poster_path;
-                    return ready;
-                });
-
-                this.build({  
-                    results: results,  
-                    title: Lampa.Lang.translate('title_watched'),
-                    line_type: 'poster' // Вертикальні постери для чистоти меню
-                });
-                
-                // Додаємо клас для CSS-фіксів тексту
-                this.render().addClass('activity-watched-custom');
-                this.render().find('.items').addClass('category-full');
-
-                return this.render();
-            };
-      
-            comp.onMore = function(data){  
-                Lampa.Activity.push({  
-                    url: 'history',  
-                    title: Lampa.Lang.translate('menu_history'),  
-                    component: 'favorite',  
-                    page: 2  
-                });
-            };
-      
-            return comp;
-        }
-      
-        // Додаємо контент на головний екран (тут залишаємо вашу логіку "як є")
-        if(Lampa.Manifest.app_digital >= 300){  
-            Lampa.ContentRows.add({  
-                name: 'watched_main',  
-                title: Lampa.Lang.translate('title_watched'),  
-                index: 0, 
-                screen: ['main'],  
-                call: (params, screen)=>{  
-                    let all = Lampa.Favorite.all();
-                    let history = all.history || [];
-                      
-                    if(!history.length) return;
-      
-                    return function(call){  
-                        let results = history.slice(0, 20).map(item => {
-                            item.background_image = item.img || item.poster || item.backdrop_path;
-                            return item;
-                        });
-
-                        call({  
-                            results: results,  
-                            title: Lampa.Lang.translate('title_watched'),
-                            card_events: true
-                        });
-                    };
-                }  
-            });
-        }  
-      
-        function add(){  
-            let button = $(`<li class="menu__item selector">  
-                <div class="menu__ico">  
-                    <svg height="36" viewBox="0 0 38 36" fill="none" xmlns="http://www.w3.org/2000/svg">  
-                        <path d="M19 2C9.6 2 2 9.6 2 19s7.6 17 17 17 17-7.6 17-17S28.4 2 19 2zm0 30c-7.2 0-13-5.8-13-13s5.8-13 13-13 13 5.8 13 13-5.8 13-13 13z" fill="currentColor"/>  
-                        <path d="M15 14v10l8-5-8-5z" fill="currentColor"/>  
-                    </svg>  
-                </div>  
-                <div class="menu__text">${manifest.name}</div>  
-            </li>`);
-      
-            button.on('hover:enter', function () {  
-                Lampa.Activity.push({  
-                    url: '',  
-                    title: manifest.name,  
-                    component: 'watched',  
-                    page: 1  
-                });
-            });
-      
-            $('.menu .menu__list').eq(0).append(button);
-        }  
-      
-        Lampa.Component.add('watched', component);
-      
-        if(window.appready) add();
-        else {  
-            Lampa.Listener.follow('app', function (e) {  
-                if (e.type == 'ready') add();
-            });
-        }
-
-        // ДОДАЄМО СТИЛІ ДЛЯ ВИПРАВЛЕННЯ НАКЛАДАННЯ ТЕКСТУ
-        $('head').append(`<style>
-            .activity-watched-custom .card__description,
-            .activity-watched-custom .card__content,
-            .activity-watched-custom .card__text-boxes {
-                display: none !important;
-            }
-            .activity-watched-custom .card__title {
-                display: block !important;
-                margin-top: 5px;
-            }
-        </style>`);
+function startPlugin() {  
+    window.plugin_watched_ready = true  
+  
+    let manifest = {  
+        type: 'video',  
+        version: '1.0.0',  
+        name: Lampa.Lang.translate('title_watched'),  
+        description: 'Shows watched movies and TV series',  
+        component: 'watched'  
     }  
       
-    if(!window.plugin_watched_ready && Lampa.Manifest.app_digital >= 242) startPlugin();
-
-})();
+    Lampa.Manifest.plugins = manifest  
+  
+    // Компонент для відображення  
+    function component(object){  
+        let comp = new Lampa.InteractionMain(object)  
+  
+        comp.create = function(){  
+            this.activity.loader(true)  
+  
+            let all = Lampa.Favorite.all()  
+            let history = all.history || []  
+  
+            this.build({  
+                results: history.slice(0, 50),  
+                title: Lampa.Lang.translate('title_watched')  
+            })  
+            return this.render()  
+        }  
+  
+        comp.onMore = function(data){  
+            Lampa.Activity.push({  
+                url: 'history',  
+                title: Lampa.Lang.translate('menu_history'),  
+                component: 'favorite',  
+                page: 2  
+            })  
+        }  
+  
+        return comp  
+    }  
+  
+    // Додаємо контент на головний екран - ПЕРШИЙ канал  
+    if(Lampa.Manifest.app_digital >= 300){  
+        Lampa.ContentRows.add({  
+            name: 'watched_main',  
+            title: Lampa.Lang.translate('title_watched'),  
+            index: 0, // ПЕРШИЙ канал на головній  
+            screen: ['main'],  
+            call: (params, screen)=>{  
+                let all = Lampa.Favorite.all()  
+                let history = all.history || []  
+                  
+                if(!history.length) return  
+  
+                return function(call){  
+                    call({  
+                        results: history.slice(0, 20), // Об'єднаний список фільмів і серіалів  
+                        title: Lampa.Lang.translate('title_watched')  
+                    })  
+                }  
+            }  
+        })  
+    }  
+  
+    function add(){  
+        let button = $(`<li class="menu__item selector">  
+            <div class="menu__ico">  
+                <svg height="36" viewBox="0 0 38 36" fill="none" xmlns="http://www.w3.org/2000/svg">  
+                    <path d="M19 2C9.6 2 2 9.6 2 19s7.6 17 17 17 17-7.6 17-17S28.4 2 19 2zm0 30c-7.2 0-13-5.8-13-13s5.8-13 13-13 13 5.8 13 13-5.8 13-13 13z"/>  
+                    <path d="M15 14v10l8-5-8-5z"/>  
+                </svg>  
+            </div>  
+            <div class="menu__text">${manifest.name}</div>  
+        </li>`)  
+  
+        button.on('hover:enter', function () {  
+            Lampa.Activity.push({  
+                url: '',  
+                title: manifest.name,  
+                component: 'watched',  
+                page: 1  
+            })  
+        })  
+  
+        $('.menu .menu__list').eq(0).append(button)  
+    }  
+  
+    // Додаємо компонент  
+    Lampa.Component.add('watched', component)  
+  
+    if(window.appready) add()  
+    else{  
+        Lampa.Listener.follow('app', function (e) {  
+            if (e.type == 'ready') add()  
+        })  
+    }  
+}  
+  
+if(!window.plugin_watched_ready && Lampa.Manifest.app_digital >= 242) startPlugin()
